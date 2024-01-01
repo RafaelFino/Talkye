727f1132c372fad4e5976631e646903fe2040406
Rafael Gottardi (Fino) <rafael.gottardi@gmail.com>
2024-01-01 12:10:58 -0300

    base modules and reqs

--- src/modules/auth.py
+++ src/modules/auth.py
@@ -1,3 +1,17 @@
+import sqlite3
 class Auth:
-    def __init__(self) -> None:
-        
+    def __init__(self, db:sqlite3) -> None:
+        self.db = None
+       
+    def auth(self, user: str, passwd:str) -> str:
+        return False
+
+    def validate(self, user:str, token:str) -> bool:
+        return False
+            
+    def testConnection(self) -> bool:
+        if self.db is None:
+            return False
+
+        return self.db.is_connected()
+            
--- src/modules/entities/user.py
+++ src/modules/entities/user.py
@@ -3,14 +3,19 @@ class User:
         self.user = user
         self.token = token
 
-    def auth(self, passwd:str) -> str:
-        return False
-
     def create(self) -> bool:
-        return False
-    
-    def validate(self) -> bool:
         return False
     
     def updatePassword(self) -> bool:
         return False
+    
+    def toJson(self):
+        return {
+            "user": self.user,
+            "token": self.token
+        }   
+    
+    def fromJson(self, json):
+        self.user = json["user"]
+        self.token = json["token"]
+    
